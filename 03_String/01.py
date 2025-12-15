2d1c10fe378423b56a0c2aab0e1f8f7dbe2db11f
mdmahboobalam <mdmahboobalam450@gmail.com>
2025-12-16 00:14:42 +0530

    fourth commit

---
+++ 03_String/01.py
@@ -0,0 +1,36 @@
+
+t='I am final year b tech student'
+u="""I am begusarai bihar"""
+
+print(t)
+print(u)
+
+a="pwskills"
+
+print(a)
+print(a[2])
+print(list(a))
+print(len(a))
+print(a[-2])
+print(a[::-1])
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.replace("s","y"))
+print(a[2:4])
+
+s="my name is md mahboob alam"
+print(s.index("name"))
+print(s.find("is"))
+print(s.find("hellow"))
+
+print(s.startswith("ma"))
+print(s.endswith("am"))
+
+print(s.split())
+print(s.split("a")) #['my n', 'me is md m', 'hboob ', 'l', 'm']
+print(s)
+
+print(s.strip())
+s = " ".join(s)
+print(s)
