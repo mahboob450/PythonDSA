b058a3161ac68872a2107bc702725ad93ebe5b3a
mdmahboobalam <mdmahboobalam450@gmail.com>
2025-12-18 21:54:03 +0530

    seventh commit

--- 04_set/01.py
+++ 04_set/01.py
@@ -1,4 +1,4 @@
-# set -> unique element, unordered, mutable
+# set -> unique element, unordered, mutable , no indexing
 
 set1={"mahboob",1,1.2,"hii","mahboob"}
 print(set1) # {'hii', 1, 'mahboob', 1.2}
@@ -7,5 +7,27 @@ print(len(set1))
 
 set1.add(100)
 print(set1)
-set1.clear()
+# set1.clear()
 print(set1)
+set1.discard(100)
+print(set1)
+
+# set operations
+#  union
+set2={1,2,3,4,5}
+set3={3,2,7,9}
+print(set2|set3)
+s=set2.union(set3)
+print(s)
+
+# intersaction
+print(set2&set3)
+
+# difference 
+print(set2-set3)
+print(set2.difference(set3))
+
+#  symmetric_difference=union - intersaction
+
+print(set2^set3)
+print(set2.symmetric_difference(set3))
