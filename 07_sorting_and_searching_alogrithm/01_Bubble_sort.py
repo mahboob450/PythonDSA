af160bd6df348f6024ef89e82181c1761988ed0f
mdmahboobalam <mdmahboobalam450@gmail.com>
2025-12-21 19:54:05 +0530

    10th commit

--- 07_sorting_and_searching_alogrithm/01_Bubble_sort.py
+++ 07_sorting_and_searching_alogrithm/01_Bubble_sort.py
@@ -1,11 +1,15 @@
 
 li=[8,1,3,2,4,5,7]
 n=len(li)
+isSorted=True
 def bubbleSort(li,n):
     for i in range(n-1):
         for j in range(n-1-i):
             if li[j]>li[j+1]:
                 li[j],li[j+1] = li[j+1],li[j]
+                isSorted=False
+        if isSorted:
+            break        
 
     return li
 
