fb40c932b22e89f5a1b283e31021ae25a193fbf2
mdmahboobalam <mdmahboobalam450@gmail.com>
2025-12-23 08:28:21 +0530

    commit

--- 06_Recursion_and_Maths/01.py
+++ 06_Recursion_and_Maths/01.py
@@ -1,11 +1,21 @@
 # recursion -> A function calling itself until a base condition is met
 #  base case -> terminating condition
 
-def sum(n):
-    if n==0:
-        return 
+# def sum(n):
+#     if n==0:
+#         return 
 
-    sum(n-1)
-    print("Hellow")   
+#     sum(n-1)
+#     print("Hellow")   
 
-sum(10)
+# sum(10)
+
+#  find sum from 1 to 10 using recursion
+
+def findSum(n):
+    if n==1:
+        return 1
+    return n+findSum(n-1)    
+
+a=findSum(10)   
+print(a) 
